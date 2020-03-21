d1896555a213eb90c4a00c2cf01d45adb9e8c109
jferLao <438127468@qq.com>
2020-03-21 23:10:09 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
     entry: {
         main: './src/index.js', //入口文件
     },
+    module: {
+        //打包规则
+        rules: [{
+            test: /\.jpg$/, //匹配规则
+            use: { //使用的loader
+                loader: 'file-loader'
+            }
+        }]
+    },
     output: { //出口文件
         filename: 'bundle.js', //打包后的文件名
         path: path.resolve(__dirname, 'dist') //__dirname是当前webpack.config.js文件的目录路径,与bundle结合最后打包在bundle文件夹中
